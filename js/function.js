edbb842b98ece6b9f7f9c3c273940c61183154b3
flopy3290 <126612319+flopy3290@users.noreply.github.com>
2023-03-07 09:21:31 -0300

    initial commit

---
+++ js/function.js
@@ -0,0 +1,17 @@
+alert("Ver Curriculum Vitae")
+
+/*documeent.getElementById('boton').onclick = function() {
+ console.log("Capturamos el evento click");
+    document.getElementById("demo").innerHTML = "Estamos probando";
+}
+*/
+document.addEventListener('click', function() {
+    console.log("Presionaste boton de contacto");
+    document.getElementById("demo").innerHTML = "Celular: 11-6562-9473. E-mail: florencia.giselle.gentile@gmail.com. LinkedIn: Flor Gentile"
+}
+
+)
+/*                        <li>Celular: 11-6562-9473</li>
+                        <li>E-mail: florencia.giselle.gentile@gmail.com</li>
+                        <li>LinkedIn: <a href="https://www.linkedin.com/in/flor-gentile-4195a055/">Flor Gentile</a></li>
+*/
